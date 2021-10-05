b8a7fff5ef6cf7d637329676d9b6966bda1a23df
Roman <the.red.lemon@yandex.ru>
2021-10-05 17:21:45 +0300

    procces06

--- AbstractJS/06.basics.js
+++ AbstractJS/06.basics.js
@@ -16,11 +16,39 @@ console.log(options.name) // обращаемся к ключу через .
 delete(options.name); // удаление свойства (значения) ключа
 console.log(options);
 
-// for in - перебор свойств объета
-// for of - для объекта работать не будет
+// for in - цикл перебора свойств объета
+// цикл for of - для объекта работать не будет
 for (let key in options) { // такой цикл будет работать столько раз, сколько есть свойств объекта
     console.log(`Свойство ${key}, имеет значение ${options[key]}`);
 }
 // вывод в консоль производится в виде строк (строчный тип данных)
 // одно из значений типа данных (colors) является тоже объектом
 // [object Object] - строковое представление объекта
+
+// перебор внутри перебора
+for (let key in options) {
+    if (typeof(options[key]) === "object") {
+        for (let i in options[key]) {
+            console.log(`Свойство ${i}, имеет значение ${options[key][i]}`);
+        } 
+    } else {
+        console.log(`Свойство ${key}, имеет значение ${options[key]}`);
+    }
+}
+
+// пример обращения к объекту в объекте
+console.log(options["colors"]["border"]); // ключи прописываются в виде строки
+
+
+// счётчик свойств (ключей) объекта
+const optionsA = {
+    name: "Test",
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: "black",
+        background: "red"
+    }
+};
+console.log(Object.keys(optionsA).length); // .keys - метод создающий массив с элементами - ключами
+// у массива как и у строк есть свойство length - позволяющее узнать количество элементов в массиве
