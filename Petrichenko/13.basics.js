f24b4335d626bc459dddaafc2338f38032a64eda
Roman <the.red.lemon@yandex.ru>
2021-11-04 21:04:24 +0300

    start13baasics

---
+++ Petrichenko/13.basics.js
@@ -0,0 +1,7 @@
+"Use strict"
+
+const box = document.getElementById("box"),
+      btns = document.getElementsByTagName("buttons"),
+      circles = document.getElementsByTagName("circle"),
+      hearts = document.querySelectorAll("heart"),
+      oneHeart = document.querySelector(".heart");
