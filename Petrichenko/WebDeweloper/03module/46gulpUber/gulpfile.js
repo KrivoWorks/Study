bd570f629a29fe782ac611159184d69bd70460b2
Roman <the.red.lemon@yandex.ru>
2022-02-09 18:02:50 +0300

    procces 46gulpUber

--- Petrichenko/WebDeweloper/03module/46gulpUber/gulpfile.js
+++ Petrichenko/WebDeweloper/03module/46gulpUber/gulpfile.js
@@ -22,6 +22,8 @@
 
 // npm install gulp --save-dev
 
+// npm i sass --save-dev
+// новый формат, ниже старый, уточнить нужен ли
 // npm i gulp-sass --save-dev
 
 // npm install -g browser-sync
@@ -38,7 +40,53 @@
 // npm i gulp-clean-css --save-dev
 // сжимает css и немного оптимизирует код
 
+// npm i gulp-rename --save-dev
+
 // ===================================================================
 
-// Настройка Gulp
-// создаём текущий файл gulpfile.js
+// Задача по созданию компилятора кода sass
+const gulp = require('gulp'),
+	  browserSync = require('browser-sync'),
+	  sass = require('gulp-sass')(require('sass')),
+	  cleanCSS = require('gulp-clean-css'),
+	  autoprefixer = require('gulp-autoprefixer'),
+	  rename = require("gulp-rename");
+
+// Настроим первую задачу - сервер
+gulp.task('server', function() {
+    browserSync({
+        server: {
+            baseDir: "src"
+        }
+    });
+	// Задача следящая за изменениями файлов
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('styles', function() {
+    return gulp.src("src/sass/**/*.+(scss|sass)")
+	// берём файлы по определённому пути
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		// запускаем компилятор кода и указываем тип стиля - compressed
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(autoprefixer())
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest("src/css")) // скомпилированный код помещаем по указанному пути
+        .pipe(browserSync.stream()); // перезапускаем браузер
+});
+
+gulp.task('watch', function() {
+    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+})
+
+// запускаем задачи с помощью ещё одной задачи
+gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+
+// чтобы запустить задачу по умолчанию достаточно просто написать gulp
+// запустится default
+// чтобы остановить сервер, нужно нажать ctrl C
+
+// чтобы запустить только определённую задачу - gulp server
+// запустится только сервер
+
+// при переносе файлов gulp в другой проект нужно прописать команду - npm i
