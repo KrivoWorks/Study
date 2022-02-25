4a21c5f67575113551dc0dceffccd5cfcee9498d
Roman <the.red.lemon@yandex.ru>
2022-02-25 15:30:45 +0300

    complite56 task

--- Petrichenko/WebDeweloper/03module/56PULSEtabs/src/js/script.js
+++ Petrichenko/WebDeweloper/03module/56PULSEtabs/src/js/script.js
@@ -35,4 +35,62 @@ $(document).ready(function(){
             }
         ]
     });
+
+    // табы
+    // https://denis-creative.com/jquery-tabs/
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        // 'li:not нажимаем на элемент у которого нет класса .catalog__tab_active
+        $(this) // this ссылается на тот элемент, на который мы нажали
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        // .addClass - добавить класс табу на который нажали
+        // .siblings().removeClass - удалить класс у всех соседних элементов если он присутствует
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+        // .closest - найти ближайщий элемент
+        // .find - внутри найти блок с классом .catalog__content
+        // .removeClass - у элементов которые нашли - удаляем класс .catalog__content_active
+        // .eq($(this).index()) - запоминаем номер таба на который нажали, например 2
+        // .addClass - определённому контенту под определённым номером добавим класс активности
+
+        // табы можно создать и с помощью bootstrap
+    });
+
+    // переключение - подробнее/назад
+    $('.test').each(function(i) {
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass("catalog-item__content_active");
+            $('.catalog-item__list').eq(i).toggleClass("catalog-item__list_active");
+        })
+    })
+    // $ получаем все элементы по селектору со страницы
+    // each - перебор каждого элемента
+    // (this) - ссылаемся на каждый элемент который перебирается
+    // .on('Click' - что будет происходить при клике
+
+    // при нажатии на ссылку - браузер переходит по адресу
+    // сейчас установлена заглушка, и при нажатии браузер обновляет страницу
+    // e.preventDefault() - можем отменить стандартное поведение
+
+    // логическое выражение - получаем класс $('.catalog-item__content')
+    // .toggleClass и если клас есть выключаем его, если нет добавляем
+    // .toggleClass("catalog-item__content_active") - указываем какой класс добавить/убрать
+
+    // i - Отвечает за номер по порядку
+
+    // добавить кнопку назад можно двумя способами
+    // либо всё скопировать и поменять класс на .catalog-item__back
+    // или всё уложить в функцию, как сделано ниже
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass("catalog-item__content_active");
+                $('.catalog-item__list').eq(i).toggleClass("catalog-item__list_active");
+            })
+        })
+    }
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
 });
