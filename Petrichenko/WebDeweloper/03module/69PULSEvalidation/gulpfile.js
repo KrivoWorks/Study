2498b25560bd02c2a2a9c69f98672dff59d1b4a5
Roman <the.red.lemon@yandex.ru>
2022-03-11 10:09:04 +0300

    'complite03module'

--- Petrichenko/WebDeweloper/03module/69PULSEvalidation/gulpfile.js
+++ Petrichenko/WebDeweloper/03module/69PULSEvalidation/gulpfile.js
@@ -32,41 +32,41 @@ gulp.task('watch', function () {
     gulp.watch("src/*.html").on('change', gulp.parallel('html'));
 });
 
-gulp.task('html', function () {
+gulp.task('html', function() {
     return gulp.src("src/*.html")
         .pipe(htmlmin({ collapseWhitespace: true }))
         .pipe(gulp.dest("dist/"));
 });
 
 // копируем все скрипты
-gulp.task('scripts', function () {
+gulp.task('scripts', function() {
     return gulp.src("src/js/**/*.js")
         .pipe(gulp.dest("dist/js"));
 });
 
 // копируем все шрифты
-gulp.task('fonts', function () {
+gulp.task('fonts', function() {
     return gulp.src("src/fonts/**/*")
         .pipe(gulp.dest("dist/fonts"));
 });
 
 // копируем все иконки
-gulp.task('icons', function () {
+gulp.task('icons', function() {
     return gulp.src("src/icons/**/*")
         .pipe(gulp.dest("dist/icons"));
 });
 
 // копируем мейлер
-gulp.task('mailer', function () {
+gulp.task('mailer', function() {
     return gulp.src("src/mailer/**/*")
         .pipe(gulp.dest("dist/mailer"));
 });
 
 // 
-gulp.task('images', function () {
+gulp.task('images', function() {
     return gulp.src("src/img/**/*")
         .pipe(imagemin())
-        .pipe(gulp.dest("dist/img "));
+        .pipe(gulp.dest("dist/img"));
 });
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'icons', 'mailer', 'images', 'html'));
