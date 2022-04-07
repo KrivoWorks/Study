d32921190ea9d3f37df9d2321831dd16134bc2fa
Home <home@gmail.com>
2022-04-07 21:47:09 +0300

    complitelesson86

--- Petrichenko/WebDeweloper/04module/82Portfolio/src/js/script.js
+++ Petrichenko/WebDeweloper/04module/82Portfolio/src/js/script.js
@@ -11,12 +11,19 @@ hamburger.addEventListener('click', () => {
 
 closeElem.addEventListener('click', () => {
 	menu.classList.remove('active');
-})
+});
 
 overlay.addEventListener('click', () => {
 	menu.classList.remove('active');
-})
+});
 
 window.addEventListener('scroll', () => {
 	menu.classList.remove('active');
-})
+});
+
+const counters = document.querySelectorAll('.skills__rating-counter'),
+	  line = document.querySelectorAll('.skills__ratings-line span');
+
+counters.forEach( (item, i) => {
+	line[i].style.width = item.innerHTML;
+});
